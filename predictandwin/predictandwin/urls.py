8134a059fee5a493f5d41a0a0672a7db9eae1b70
ayousocrazy <ayousocrazy@gmail.com>
2026-07-01 22:11:06 +0545

    Change url config

--- predictandwin/predictandwin/urls.py
+++ predictandwin/predictandwin/urls.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Main app
+    path('', include('main.urls')),
+
+    # API app
+    path('api/', include('api.urls')),
 ]
+
+# Media files
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
